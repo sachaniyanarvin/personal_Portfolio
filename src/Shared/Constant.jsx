90fdebe6820c4bbbc334d473f7d906a4f4beb4e0
sachaniyanarvin <sachaniyanarvin21@gmail.com>
2025-04-19 02:54:49 +0530

    content folder from shared add in

---
+++ src/Shared/Constant.jsx
@@ -0,0 +1,54 @@
+export const currentCompanyName = 'Fidelity National Financial India';
+export const currentRole = 'Full Stack Engineer';
+export const currentExperience = '4.1 Years';
+export const currentMobileNo = '+919879582006';
+export const currentEmail = 'sachaniyanarvin21@gmail.com';
+export const currentLinkedinUrl = 'https://www.linkedin.com/in/narvin-sachaniya/';
+export const currentGithubUrl = 'https://github.com/sachaniyanarvin';
+export const currentzcompanyStartDate = 'February 2023';
+export const currentLocation = 'Ahemdabad';
+export const oldCompanyHistory = [
+    {
+        name: 'Kantar Analytics Practice',
+        designation: 'Software Developer',
+        startDate: 'November 2019',
+        endDate: 'February 2023',
+        location: 'Bangalore',
+        isCurrentCompany : false,
+        id : 1
+    },
+    {
+        name: 'Fidelity National Financial India',
+        designation: 'Software Engineer',
+        startDate: 'February 2023',
+        location: 'Bangalore',
+        isCurrentCompany : true,
+        id : 2
+    }
+];
+
+export const Languages = [
+    {
+        name: 'DotNet'
+    },
+    {
+        name: 'Reactjs'
+    },
+    {
+        name:'Javascript'
+    },
+    {
+        name:'Sql'
+    },
+    {
+        name:'Angular'
+    },
+    {
+        name:'HTML'
+    },
+    {
+        name:'CSS'
+    }
+]
+
+
