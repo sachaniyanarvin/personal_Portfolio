e6bf078595283e36f5d5d76c4385ed205b57b00f
sachaniyanarvin <sachaniyanarvin21@gmail.com>
2025-05-22 11:47:52 +0530

    In certificate page involve a new certificate

--- src/Pages/Certificates.jsx
+++ src/Pages/Certificates.jsx
@@ -11,7 +11,7 @@ function Certificates() {
 
     const certificates = [
         { title: 'Certificate 1', src: 'https://gateway.pinata.cloud/ipfs/QmdWVLmock7EY3S4yMH23mjypkrqRZEnm4XsA59ZHHa1XK' },
-        { title: 'Certificate 2', src: 'https://gateway.pinata.cloud/ipfs/QmXdBipGGsPi32pB4mCPxgSUA9J1KELcDnEJK4it3iZGJp' },
+        { title: 'Certificate 2', src: 'https://gateway.pinata.cloud/ipfs/Qmc4LVuwhJQCS5WJegdnZkLNWeQUMibhspSgFcEfdGzUzn' },
         { title: 'Certificate 3', src: 'https://gateway.pinata.cloud/ipfs/QmTpiHZ7zFPA1tfngMbLEfhBYf7E3LDyy7zNQrRSbZMXvH' },
         { title: 'Certificate 4', src: 'https://gateway.pinata.cloud/ipfs/Qmenqg1of5HMWQTGQCHTDEh515tAimc4TvrcG2tYaBHVgz' },
         { title: 'Certificate 5', src: 'https://gateway.pinata.cloud/ipfs/QmXq4waGkQd6Sv5YfPLhhDychRZuhpJA8dL313H61mxcNV' },
@@ -24,7 +24,8 @@ function Certificates() {
         { title: 'Certificate 12', src: 'https://gateway.pinata.cloud/ipfs/QmS4HUeNHrQcfEnuz9sp9LqQDEWUM35zvbuoMyESETowDA' },
         { title: 'Certificate 13', src: 'https://gateway.pinata.cloud/ipfs/QmaMUjcfVCM8xGxw584LeVhkwn7CkhuymmKfWGZf1JHnYe' },
         { title: 'Certificate 14', src: 'https://gateway.pinata.cloud/ipfs/QmUgUvWq126m8nDyxZEc5HV1o4tYNuWz3USCnxVbsGhbY4' },
-        { title: 'Certificate 15', src: 'https://gateway.pinata.cloud/ipfs/QmS4bYVmcSeJ3Tkfadk3cGaHhQbhwwcTtJHQQwPMwLGsYd' }
+        { title: 'Certificate 15', src: 'https://gateway.pinata.cloud/ipfs/QmS4bYVmcSeJ3Tkfadk3cGaHhQbhwwcTtJHQQwPMwLGsYd' },
+        { title: 'Certificate 16', src: 'https://gateway.pinata.cloud/ipfs/QmXdBipGGsPi32pB4mCPxgSUA9J1KELcDnEJK4it3iZGJp'}
     ];
 
     const handleClick = (src) => {
